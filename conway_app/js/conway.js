0f08781643aad84ebeecd577a7cbbedc0a715311
Nadia Mounzih <nmounzih@yahoo.com>
2017-04-16 19:55:30 -0400

    Fixed js file

--- conway_app/js/conway.js
+++ conway_app/js/conway.js
@@ -1,69 +1,39 @@
+buildGameBoard(10, 10);
+
+
+$('#start').click(updateCells);
+
+
+
 function cellClicked(e){
-  var $src = $(e.target);
-  $src.addClass('clicked');
-  console.log($src);
-  let neighbors = getNeighbors(e.target);
-  let count = countLiveNeighbors(neighbors);
-  console.log(count);
-  if(count < 2 || count > 3){
-    $src.addClass('dead');
+  let $cell = $(e.target);
+  // $cell.addClass('clicked');
+  if($cell.hasClass('clicked')){
+    $cell.removeClass('clicked');
+  } else {
+    $cell.addClass('clicked');
   }
-  else if(count===3 && $src.hasClass('dead')){//might never happen
-    $src.addClass('clicked');
-  }
+  let y = e.target.parentNode.rowIndex;
+  let x = e.target.cellIndex;
+
 }
 
-function deadOrAlive(cell){
-  
-}
+  // let neighbors = getNeighbors(e.target);
+  // let count = countLiveNeighbors(neighbors);
+  // console.log(neighbors);
+  // console.log(count);
+  // if(count < 2 || count > 3){
+  //   $src.addClass('dead');
+  // }
+  // else if(count===3 && $src.hasClass('dead')){//might never happen
+  //   $src.addClass('clicked');
+  // }
+
 
 function getCell(x, y){
   return $('#cell_' + x + '_' + y);
+  console.log('getcell');
 }
-
-function getNeighbors(currentCell){
-  let y = currentCell.parentNode.rowIndex;
-  let x = currentCell.cellIndex;
-  let xe = x-1;
-  let xw = x+1;
-  let yn = y-1;
-  let ys = y+1;
-  let ne = getCell(xe, yn);
-  let n = getCell(x, yn);
-  let nw = getCell(xw, yn);
-  let e = getCell(xe, y);
-  let w = getCell(xw, y);
-  let se = getCell(xe, ys);
-  let s = getCell(x, ys);
-  let sw = getCell(xw, ys);
-
-  return [ne, n, nw, e, w, se, s, sw];
-}
-
-function countLiveNeighbors(neighbors){
-  let count = 0;
-  for(let i =0; i < neighbors.length; i++){
-    if(neighbors[i].hasClass('clicked')){
-      count ++;
-    }
-  }
-  return count;
-}
-
-// function killCell(e, neighbors){
-//   let count = 0;
-//   for(let i = 0; i < neighbors.length; i++){
-//     if(neighbors[i].hasClass('clicked')){
-//       count ++;
-//     if(count > 0){
-//       e.target.addClass('dead');
-//     }
-//     }
-//     }
-//     return e.target
-//   }
-
-
 
 function buildGameBoard(x, y){
   let $board = $('<table>');
@@ -81,5 +51,79 @@ function buildGameBoard(x, y){
   console.log($board);
   }
 
+function toggleCells(x, y){
+  for(var i = 0; i < x; i++){
+    for(var j = 0; j < y; j++){
+      let $cell = getCell(i, j);
+      // let neighbors = getNeighbors($cell);
+      let count = countLiveNeighbors(i, j);
+      if($cell.hasClass('clicked')){
+        if(count < 2 || count > 3){
+          $cell.addClass('dead');
+        }
+      } else {
+        if(count === 3) {
+          $cell.addClass('dead_to_live')
+        }
+      }
+    }
+  }
+  console.log('hey')
+}
 
-buildGameBoard(10, 10)
+
+function updateCells(x, y){
+  toggleCells(x, y);
+  for(var i = 0; i < x; i++){
+    for(var j = 0; j < y; j++){
+      let $cell = getCell(i, j);
+      if($cell.hasClass('dead')){
+        $cell.removeClass('clicked');
+        $cell.removeClass('dead');
+      } if($cell.hasClass('dead_to_live')){
+        $cell.addClass('clicked');
+        $cell.removeClass('dead_to_live');
+      }
+      // let neighbors = getNeighbors($cell);
+      // let count = countLiveNeighbors(neighbors);
+      // if(count < 2 || count > 3){
+      //   $src.addClass('dead');
+      // }
+      // else if(count===3 && $src.hasClass('dead')){  //might never happen
+      //   $src.addClass('clicked');
+      // }
+    }
+
+  }
+  // var $src = $(currentCell.hasClass('clicked'));
+  console.log('hi mom');
+}
+
+function getNeighbors(i, j){
+  let y = i;
+  let x = j;
+  let xe = x-1;
+  let xw = x+1;
+  let yn = y-1;
+  let ys = y+1;
+  let $ne = getCell(xe, yn);
+  let $n = getCell(x, yn);
+  let $nw = getCell(xw, yn);
+  let $e = getCell(xe, y);
+  let $w = getCell(xw, y);
+  let $se = getCell(xe, ys);
+  let $s = getCell(x, ys);
+  let $sw = getCell(xw, ys);
+
+  return [$ne, $n, $nw, $e, $w, $se, $s, $sw];
+}
+
+function countLiveNeighbors(neighbors){
+  let count = 0;
+  for(let i =0; i < neighbors.length; i++){
+    if(neighbors[i].hasClass('clicked')){
+      count ++;
+    }
+  }
+  return count;
+}
