b6f5b95cf12932da9cd02b891e70433004a5b7e4
Nadia Mounzih <nmounzih@yahoo.com>
2017-05-04 21:44:54 -0400

    Added setInterval

--- conway_app/js/conway.js
+++ conway_app/js/conway.js
@@ -3,44 +3,45 @@ let y = 20;
 $('#go').click(start);
 
 function cellClicked(e){
-  let $cell = $(e.target);
-  if($cell.hasClass('clicked')){
-    $cell.removeClass('clicked');
-  } else {
-    $cell.addClass('clicked');
+  var $src = $(e.target);
+  $src.addClass('clicked');
+  console.log($src);
+  let neighbors = getNeighbors(e.target);
+  let count = countLiveNeighbors(neighbors);
+  console.log(count);
+  if(count < 2 || count > 3){
+    $src.addClass('dead');
   }
-  let y = e.target.parentNode.rowIndex;
-  let x = e.target.cellIndex;
-
+  else if(count===3 && $src.hasClass('dead')){
+    $src.addClass('clicked');
+  }
 }
 
-
-function getCell(i, j){
-  return $('#cell_' + i + '_' + j);
+function getCell(x, y){
+  return $('#cell_' + x + '_' + y);
 }
 
-function getNeighbors(i, j){
-  let y = j;
-  let x = i;
+function getNeighbors(currentCell){
+  let y = currentCell.parentNode.rowIndex;
+  let x = currentCell.cellIndex;
   let xe = x-1;
   let xw = x+1;
   let yn = y-1;
   let ys = y+1;
-  let $ne = getCell(xe, yn);
-  let $n = getCell(x, yn);
-  let $nw = getCell(xw, yn);
-  let $e = getCell(xe, y);
-  let $w = getCell(xw, y);
-  let $se = getCell(xe, ys);
-  let $s = getCell(x, ys);
-  let $sw = getCell(xw, ys);
+  let ne = getCell(xe, yn);
+  let n = getCell(x, yn);
+  let nw = getCell(xw, yn);
+  let e = getCell(xe, y);
+  let w = getCell(xw, y);
+  let se = getCell(xe, ys);
+  let s = getCell(x, ys);
+  let sw = getCell(xw, ys);
 
-  return [$ne, $n, $nw, $e, $w, $se, $s, $sw];
+  return [ne, n, nw, e, w, se, s, sw];
 }
 
-function countLiveNeighbors(i, j){
+function countLiveNeighbors(neighbors){
   let count = 0;
-  let neighbors = getNeighbors(i, j);
   for(let i =0; i < neighbors.length; i++){
     if(neighbors[i].hasClass('clicked')){
       count ++;
@@ -62,50 +63,28 @@ function buildGameBoard(x, y){
     }
   }
   $('#gameBoard').append($board);
+  console.log($board);
   }
-
-function toggleCells(x, y){
-  for(var j = 0; j < y; j++){
-    for(var i = 0; i < x; i++){
-      let $cell = getCell(i, j);
-      // let neighbors = getNeighbors($cell);
-      let count = countLiveNeighbors(i, j);
-      if($cell.hasClass('clicked')){
-        if(count < 2 || count > 3){
-          $cell.addClass('dead');
-        }
-      } else {
-        if(count === 3) {
-          $cell.addClass('dead_to_live')
-        }
-      }
-    }
-  }
-}
-
 
 function updateCells(){
-  toggleCells(x, y);
   for(var j = 0; j < y; j++){
     for(var i = 0; i < x; i++){
       let $cell = getCell(i, j);
       if($cell.hasClass('dead')){
         $cell.removeClass('clicked');
         $cell.removeClass('dead');
-      } if($cell.hasClass('dead_to_live')){
+      } if($cell.hasClass('dead')){
         $cell.addClass('clicked');
-        $cell.removeClass('dead_to_live');
+        $cell.removeClass('deadx');
       }
     }
 
   }
-  // var $src = $(currentCell.hasClass('clicked'));
   console.log('hi mom');
 }
 
 function start(x, y){
   setInterval(updateCells, 500);
 }
-
 
 buildGameBoard(x, y);
